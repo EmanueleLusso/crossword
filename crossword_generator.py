65622a2ad5f7ee6425cc786cf97944bded18f3c7
cathyli-j <jl3253@cornell.edu>
2021-04-26 15:37:08 -0500

    minor improvements, some bug fixes

--- crossword_generator.py
+++ crossword_generator.py
@@ -7,11 +7,11 @@ class Crossword(object):
         self.rows = rows
         self.words = words
         self.current_build = [] #All the words currently in the crossword
-        self.current_build_visualizer = [[0] * cols for i in range(rows)] #A matrix of characters and 0s where 0s represent a spot without a character in it 
+        self.current_build_visualizer = [[0] * cols for i in range(rows)] #A matrix representation of crossword. 0 represents a space
         self.end_build = False
 
     def score(self, row, col, word, horizontal): 
-        curr_score = 0 # Just counts how many intersections there are. Returns NaN if the word does not fit
+        curr_score = 0 #Score = how many intersections there are with existing words. Returns NaN if the word does not fit 
         if horizontal: 
             if col + len(word) > self.cols:
                 return np.nan
@@ -35,6 +35,7 @@ class Crossword(object):
         return curr_score
 
     def build(self):
+        print(self.current_build_visualizer)
         while not self.end_build: 
             if len(self.current_build) == 0: #For the first word, add the longest word that fits 
                 for word in self.words: 
@@ -52,24 +53,23 @@ class Crossword(object):
                                 scores[i] = self.score(row, col, word[0], horizontal)
                             try:
                                 i_max = np.nanargmax(scores)
-                            except ValueError: 
+                            except ValueError: #nanargmax returns ValueError when presented with a slice of only NaN
                                 continue 
                             new_word = Entry(self.words[i_max][0], self.words[i_max][1])
                             self.add(new_word, row, col, horizontal)
                             self.words.pop(i_max)
-                self.end_build = True # Doesn't do anything right now but could probably use this as some sort of "timeout" break switch somewhere
+                self.end_build = True # Doesn't really do anything right now but could probably use this as some sort of "timeout" break switch somewhere
 
-    def build_simple(self):
-        pass
-
+    #Add an entry to the board
     def add(self, entry, row, col, horizontal):
         entry.start_row = row 
         entry.start_col = col
         entry.horizontal = horizontal
         self.current_build.append(entry)
+        #Add entry to visualizer
         if horizontal: 
             print('adding horizontal entry')
-            self.current_build_visualizer[row][col:col + len(entry.word)-1]  = entry.word
+            self.current_build_visualizer[row][col:(col+len(entry.word))]  = entry.word
         else: 
             print('adding vertical entry')
             for i in range(len(entry.word)):
@@ -83,22 +83,25 @@ class Crossword(object):
         for entry in self.current_build: 
             print(entry.word + ": " + entry.clue + "; At " + str(entry.start_row) + "," + str(entry.start_col) + " Horizontal = " + str(entry.horizontal))
 
-
+# Class for each entry in the crossword
 class Entry(object): 
-    def __init__(self, word=None, text=None):
+    def __init__(self, word, clue):
         self.word = word
-        self.clue = text
+        self.clue = clue
         self.start_row = None
         self.start_col = None
         self.horizontal = None 
 
-
-def organize_words(words):
+# shuffle before starting, remove words that will not fit in the crossword to save computation time
+def organize_words(words, crossword_size):
+    words[:] = [word for word in words if len(word[0]) <= crossword_size]
     random.shuffle(words)
     words.sort(key=lambda x: len(x[0]), reverse=True)
 
-wordbank = [['abracadabra', 'filler'], ["apple", 'a fruit'], ['ant', 'a pest'], ['water', 'a beverage'], ['pretzel', 'a snack']]
+wordbank = [['abracadabra', 'filler'], ["apple", 'a fruit'], ['ant', 'a pest'], ['task', 'something to do'], ['water', 'a beverage'], ['pretzel', 'a snack']]
+crossword_size = 7
 
-a = Crossword(5, 5, wordbank)
+organize_words(wordbank, crossword_size)
+a = Crossword(crossword_size, crossword_size, wordbank)
 a.build()
 a.print_data()
